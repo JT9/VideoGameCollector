b74ab4d8ac3d043daf2b70f07f7974b6ecad64f7
JT9 <kasusaku@gmail.com>
2017-10-03 23:15:23 -0700

    Initial Commit

---
+++ VideoGameCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoGameCollector
+//
+//  Created by C T on 10/3/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
