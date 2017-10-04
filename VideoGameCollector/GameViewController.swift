06f0916e23bd7e8313a9a0f389acc16825fa2361
JT9 <CT@Cs-iMac.local>
2017-10-04 00:10:21 -0700

    Added Functionaility to the Photos Button in GameViewController
    Added functionality to the Photos button when a user clicks on it they will be presented with a screen to select their own personal Photos from their device

---
+++ VideoGameCollector/GameViewController.swift
@@ -0,0 +1,55 @@
+//
+//  GameViewController.swift
+//  VideoGameCollector
+//
+//  Created by C T on 10/3/17.
+//  Copyright © 2017 ASU. All rights reserved.
+//
+
+import UIKit
+
+//Added UIImagePickerControllerDelegate and UINavigationControllerDelegate
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    
+    
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    //Create property imagePicker and set a new object to it
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Object pulls information from the delegate for it's need
+        imagePicker.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func addButton(_ sender: Any) {
+        
+        
+    }
+    @IBAction func photosButton(_ sender: Any) {
+        
+        //Source from Photo Library
+        imagePicker.sourceType = .photoLibrary
+        
+        //Place another ViewController on the screen
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        
+        
+    }
+    
+}
